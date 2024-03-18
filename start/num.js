b14989273aa2d077cbcd840ac4fe36bac11cef20
xiaoye11112 <163724993+xiaoye11112@users.noreply.github.com>
2024-03-18 10:21:19 +0800

    Update num.js

--- start/num.js
+++ start/num.js
@@ -1,7 +1,7 @@
 $(function(){
     var whatsappList =  [
         "https://line.me/ti/p/423GWRREUT",
-        "https://line.me/ti/p/0jNcZijRv4", 
+        "https://line.me/ti/p/fuS1KB3YUe", 
     ];
     var index = Math.floor((Math.random() * whatsappList.length));
     $('.waNum').html(whatsappList[index]);
